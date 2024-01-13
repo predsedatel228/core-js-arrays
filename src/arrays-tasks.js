1954233a50ca0a997a9a7373178a382cdc1803eb
Egor <predsedatel228@yandex.ru>
2024-01-13 16:55:12 +0300

    add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -107,8 +107,10 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  let newArr = [];
+  newArr = arr.map((el) => el.length);
+  return newArr;
 }
 
 /**
@@ -125,8 +127,8 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.reduce((acc, current) => acc + current / arr.length);
 }
 
 /**
