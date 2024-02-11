147a750b1dca08cdd5c0e4b9779fb8d2f66c1191
Egor Panevin <predsedatel228@yandex.ru>
2024-02-11 21:29:57 +0300

    add solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -348,8 +348,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const answer = Array.from(
+    { length: Math.ceil(arr.length / chunkSize) },
+    (el, index) => arr.slice(index * chunkSize, (index + 1) * chunkSize)
+  );
+  return answer;
 }
 
 /**
